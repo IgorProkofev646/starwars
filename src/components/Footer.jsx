1970b1c071da32c64e0d477fe46c3fa453093d72
Igor <porsche.igor646@gmail.com>
2025-01-28 19:42:24 +0200

    sw-tailwind

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className={'bg-grey-color h-20 rounded-b-2xl grid grid-cols-12 items-center'}>
+            <div className={'col-start-5 col-span-2 bg-red-color border-black border-2 rounded-md px-3 text-center py-2 cursor-pointer hover:bg-red-500 hover:text-white'}>Send me an <span
+                className={"text-black uppercase"}>email</span></div>
+        </footer>
+    );
+};
+
+export default Footer;
